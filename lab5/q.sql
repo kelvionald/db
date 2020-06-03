cdb804ba3121bee3009320da11bd68706a4bdea3
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-06-03 13:15:21 +0300

    5 лаба

--- lab5/q.sql
+++ lab5/q.sql
@@ -1,37 +1,13 @@
 use lab5;
 
 -- 1. Добавить внешние ключи.
-
-ALTER TABLE [dbo].[dealer]  WITH CHECK ADD  CONSTRAINT [FK_dealer_company] FOREIGN KEY([id_company])
-REFERENCES [dbo].[company] ([id_company])
-GO
-ALTER TABLE [dbo].[dealer] CHECK CONSTRAINT [FK_dealer_company]
-GO
-ALTER TABLE [dbo].[order]  WITH CHECK ADD  CONSTRAINT [FK_order_dealer] FOREIGN KEY([id_dealer])
-REFERENCES [dbo].[dealer] ([id_dealer])
-GO
-ALTER TABLE [dbo].[order] CHECK CONSTRAINT [FK_order_dealer]
-GO
-ALTER TABLE [dbo].[order]  WITH CHECK ADD  CONSTRAINT [FK_order_pharmacy] FOREIGN KEY([id_pharmacy])
-REFERENCES [dbo].[pharmacy] ([id_pharmacy])
-GO
-ALTER TABLE [dbo].[order] CHECK CONSTRAINT [FK_order_pharmacy]
-GO
-ALTER TABLE [dbo].[order]  WITH CHECK ADD  CONSTRAINT [FK_order_production] FOREIGN KEY([id_production])
-REFERENCES [dbo].[production] ([id_production])
-GO
-ALTER TABLE [dbo].[order] CHECK CONSTRAINT [FK_order_production]
-GO
-ALTER TABLE [dbo].[production]  WITH CHECK ADD  CONSTRAINT [FK_production_company] FOREIGN KEY([id_company])
-REFERENCES [dbo].[company] ([id_company])
-GO
-ALTER TABLE [dbo].[production] CHECK CONSTRAINT [FK_production_company]
-GO
-ALTER TABLE [dbo].[production]  WITH CHECK ADD  CONSTRAINT [FK_production_medicine] FOREIGN KEY([id_medicine])
-REFERENCES [dbo].[medicine] ([id_medicine])
-GO
-ALTER TABLE [dbo].[production] CHECK CONSTRAINT [FK_production_medicine]
-GO
+  
+ALTER TABLE dealer ADD FOREIGN KEY (id_company) REFERENCES company (id_company);
+ALTER TABLE production ADD FOREIGN KEY (id_company) REFERENCES company (id_company);
+ALTER TABLE production ADD FOREIGN KEY (id_medicine) REFERENCES medicine (id_medicine);
+ALTER TABLE [order] ADD FOREIGN KEY (id_dealer) REFERENCES dealer (id_dealer);
+ALTER TABLE [order] ADD FOREIGN KEY (id_production) REFERENCES production (id_production);
+ALTER TABLE [order] ADD FOREIGN KEY (id_pharmacy) REFERENCES pharmacy (id_pharmacy);
 
 
 -- 2. Выдать информацию по всем заказам лекарства “Кордерон” компании “Аргус” с 
@@ -47,12 +23,72 @@ WHERE
 	m.name = 'Кордерон' AND
 	c.name = 'Аргус'
 
+
 -- 3. Дать список лекарств компании “Фарма”, на которые не были сделаны заказы
 -- до 25 января.
+
+SELECT m.name 
+FROM medicine AS m
+LEFT JOIN production AS pr ON pr.id_medicine = m.id_medicine
+LEFT JOIN company AS c ON c.id_company = pr.id_company
+LEFT JOIN [order] AS o ON o.id_production = pr.id_production
+WHERE c.name = 'Фарма' AND pr.id_production NOT IN (
+	SELECT o.id_production 
+	FROM [order] AS o
+	WHERE o.date < '25-01-2019'
+)
+GROUP BY m.name
+
+
 -- 4. Дать минимальный и максимальный баллы лекарств каждой фирмы, которая
 -- оформила не менее 120 заказов.
+
+SELECT
+	c.name, 
+	MIN(pr.rating) AS min_rating, 
+	MAX(pr.rating) AS max_rating
+FROM production AS pr
+LEFT JOIN company AS c ON pr.id_company = c.id_company
+LEFT JOIN [order] AS o ON pr.id_production = o.id_production
+GROUP BY c.id_company, c.name
+HAVING COUNT(o.id_order) >= 120
+
+
 -- 5. Дать списки сделавших заказы аптек по всем дилерам компании “AstraZeneca”.
 -- Если у дилера нет заказов, в названии аптеки проставить NULL.
+
+SELECT 
+	d.id_dealer, 
+	d.name,
+	ph.name
+FROM dealer AS d
+LEFT JOIN company AS c ON d.id_company = c.id_company
+LEFT JOIN [order] AS o ON o.id_dealer = d.id_dealer 
+LEFT JOIN pharmacy AS ph ON ph.id_pharmacy = o.id_pharmacy
+WHERE c.name = 'AstraZeneca'
+ORDER BY d.id_dealer
+
+
 -- 6. Уменьшить на 20% стоимость всех лекарств, если она превышает 3000, а
 -- длительность лечения не более 7 дней.
+
+UPDATE production
+SET production.price = production.price * 0.8
+WHERE production.id_production IN (
+	SELECT pr.id_production
+	FROM production AS pr
+	LEFT JOIN medicine ON pr.id_medicine = medicine.id_medicine
+	WHERE pr.price > 3000 AND medicine.cure_duration <= 7
+)
+
+
 -- 7. Добавить необходимые индексы.
+
+CREATE NONCLUSTERED INDEX [IX_dealer_id_company] ON dealer (id_company)
+CREATE NONCLUSTERED INDEX [IX_company_name] ON company (name)
+CREATE NONCLUSTERED INDEX [IX_medicine_name] ON medicine (name)
+CREATE NONCLUSTERED INDEX [IX_production_id_company] ON production (id_company)
+CREATE NONCLUSTERED INDEX [IX_production_id_medicine] ON production (id_medicine)
+CREATE NONCLUSTERED INDEX [IX_order_id_production] ON [order] (id_production)
+CREATE NONCLUSTERED INDEX [IX_order_id_dealer] ON [order] (id_dealer)
+CREATE NONCLUSTERED INDEX [IX_order_id_pharmacy] ON [order] (id_pharmacy)
